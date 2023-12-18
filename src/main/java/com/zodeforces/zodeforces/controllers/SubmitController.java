fcb5c01e7b7152c2f8e54f3ae616feb04ec94ad1
Eyad-Hantouli <eyadhantouli0@gmail.com>
2023-12-18 10:28:40 +0300

    controllers init

---
+++ src/main/java/com/zodeforces/zodeforces/controllers/SubmitController.java
@@ -0,0 +1,9 @@
+package com.zodeforces.zodeforces.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/submits")
+public class SubmitController {
+}
